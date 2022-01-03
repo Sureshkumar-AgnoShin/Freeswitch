4b601253f2fabcc14de56be55250135ff443e871
agnouser <88184933+agnouser@users.noreply.github.com>
2022-01-03 15:27:43 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 apt-get update && apt-get install -yq gnupg2 wget lsb-release
 
-cd depends
+wget -O - https://files.freeswitch.org/repo/deb/debian-release/fsstretch-archive-keyring.asc | apt-key add -
+ 
+echo "deb http://files.freeswitch.org/repo/deb/debian-release/ `lsb_release -sc` main" > /etc/apt/sources.list.d/freeswitch.list
+echo "deb-src http://files.freeswitch.org/repo/deb/debian-release/ `lsb_release -sc` main" >> /etc/apt/sources.list.d/freeswitch.list
+ 
 
-dpkg -i /*.deb
+#cd depends
 
-cd ..
+#apt-get install ./*.deb
+
+#cd ..
 
 apt-get update -y
 
