32127bfd5a566e28f50c54b346f9bbd17c798946
Sureshkumar <87412756+Sureshkumar-AgnoShin@users.noreply.github.com>
2022-11-22 11:23:30 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 apt-get update && apt-get install -yq gnupg2 wget lsb-release
 
-#wget -O - https://files.freeswitch.org/repo/deb/debian-release/fsstretch-archive-keyring.asc | apt-key add -
+#wget --http-user=signalwire --http-password=pat_vEUo5hZx9QFL2uU3ttDD6DAv -O /usr/share/keyrings/signalwire-freeswitch-repo.gpg https://freeswitch.signalwire.com/repo/deb/debian-release/signalwire-freeswitch-repo.gpg
  
-#echo "deb http://files.freeswitch.org/repo/deb/debian-release/ `lsb_release -sc` main" > /etc/apt/sources.list.d/freeswitch.list
-#echo "deb-src http://files.freeswitch.org/repo/deb/debian-release/ `lsb_release -sc` main" >> /etc/apt/sources.list.d/freeswitch.list
+#echo "machine freeswitch.signalwire.com login signalwire password pat_vEUo5hZx9QFL2uU3ttDD6DAv" > /etc/apt/auth.conf
+#echo "deb [signed-by=/usr/share/keyrings/signalwire-freeswitch-repo.gpg] https://freeswitch.signalwire.com/repo/deb/debian-release/ `lsb_release -sc` main" > /etc/apt/sources.list.d/freeswitch.list
+#echo "deb-src [signed-by=/usr/share/keyrings/signalwire-freeswitch-repo.gpg] https://freeswitch.signalwire.com/repo/deb/debian-release/ `lsb_release -sc` main" >> /etc/apt/sources.list.d/freeswitch.list
  
 
 #cd depends
